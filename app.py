c60751653489233c0328edbad6f7fb32a730c83f
terrydang <terrydang.cn@gmail.com>
2023-11-24 12:08:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, request, jsonify
-import logging
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return "Hello world!"
-
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
+    return Response(message)
 
 if __name__ == '__main__':
-    app.run(port=8080, host="0.0.0.0")
+    port = int(os.environ.get("PORT"))
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
